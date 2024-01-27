0d318807f1e43f9a06bbce7eeed2560fc9b72b7d
zhazhajun1 <141290199+zhazhajun1@users.noreply.github.com>
2024-01-27 09:08:24 +0800

    V4.0
    V4.0发布

---
+++ 开源脚本v4.0.sh
@@ -0,0 +1,768 @@
+#!/bin/bash
+
+# 定义变量
+AI_URL="http://web6899.hk11.epep.eu.org/"
+
+# 连接服务器
+echo "正在连接服务器..."
+echo "Connecting to the server…"
+
+response=$(curl -X POST --insecure -H "Content-Type: application/json" -d '{"url": "'"$AI_URL"'"}' "http://web6899.hk11.epep.eu.org/")
+
+echo "连接成功！"
+echo "Connect successfully!"
+#获取ip
+echo "你的ip是："
+echo "Your ip is:"
+curl http://myip4.ipip.net
+sleep 2
+clear
+
+# 提示输入选项
+echo "请选择一个选项："
+echo "Please select an option："
+
+    echo "1. 中文Chinese"
+    echo "2. 英文English"
+    echo "3. 退出Sign out"
+
+    read choice
+
+    case $choice in
+        1)
+        #!/bin/bash
+
+#前言
+echo "你好，此脚本为开源脚本"
+sleep 0.1
+echo "所有内容均可复制!"
+sleep 0.05
+echo "不能倒卖!"
+sleep 0.05
+echo "不能倒卖!!"
+sleep 0.05
+echo "不能倒卖!!!"
+sleep 0.1
+echo "作者快手：ba20111022"
+sleep 0.1
+echo "接下来进入程序"
+sleep 2
+clear
+#密码程序
+# 提示输入密码
+echo "请输入密码："
+read -s password
+
+# 检查密码是否正确
+if [[ "$password" == "zhazhajun" ]]; then
+    # 提示登录成功
+    echo "登录成功！"
+else
+    # 如果密码错误，退出程序  
+    echo "密码错误，程序退出"
+    exit 1
+fi
+clear
+# 提示输入选项
+echo "请选择一个选项："
+    echo "1. 时间"
+    echo "2. 日历"
+    echo "3. 巴ba"
+    echo "4. 网页源码获取"
+    echo "5. 计算器"
+    echo "6. ip获取"
+    echo "7. 退出"
+
+    read choice
+clear
+    case $choice in
+        1)
+            #1
+    #加载程序
+echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "文件内容加载成功！"
+echo "请稍等!"
+sleep 0.5
+# 获取当前时间
+    current_time=$(date +%H:%M)
+#输出当前时间
+echo 当前时间为：$(date +%H:%M)
+echo "加载成功！"
+            ;;
+        2)
+            echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "文件内容加载成功！"
+echo "请稍等!"
+sleep 0.5
+cal
+echo "加载成功！"
+
+            ;;
+        3)
+        echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "文件内容加载成功！"
+echo "请稍等!"
+sleep 0.5
+            while true
+do
+echo "巴ba❤️nb666"#自己使用内容可更改
+set +x
+let "j=j+1"
+done
+            
+            ;;
+        4)
+                echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "文件内容加载成功！"
+echo "请稍等!"
+sleep 0.5
+           #!/bin/bash
+echo "输入网址:"
+read url
+if [ $? -eq 0 ]; then
+curl -s "$url" > index.html
+echo "源码已写入index.html"
+else
+   echo "请求出错！"
+fi
+            ;;
+        5)
+        echo "欢迎使用计算器，计算器循环执行！"
+        sleep 0.55
+        clear
+        echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "文件内容加载成功！"
+echo "请稍等!"
+sleep 0.5
+#!/system/bin/sh
+while true
+do
+# 定义函数实现加法
+addition() {
+    result=$(expr $1 + $2)
+    echo "结果: $result"
+}
+
+# 定义函数实现减法
+subtraction() {
+    result=$(expr $1 - $2)
+    echo "结果: $result"
+}
+
+# 定义函数实现乘法
+multiplication() {
+    result=$(expr $1 \* $2)
+    echo "结果: $result"
+}
+
+# 定义函数实现除法
+division() {
+    result=$(echo "scale=2; $1 / $2" | bc)
+    echo "结果: $result"
+}
+
+# 读取用户输入的两个数字
+echo "请输入第一个数字: "
+read num1
+echo "请输入第二个数字: "
+read num2
+
+# 显示操作选项给用户
+echo "请选择计算方法："
+echo "1. 加法"
+echo "2. 减法"
+echo "3. 乘法"
+echo "4. 除法"
+
+# 读取用户选择的操作
+echo "请输入计算方法: "
+read operation
+
+# 根据用户选择的操作调用相应的函数进行计算并显示结果
+case $operation in
+    1)
+        addition $num1 $num2
+        ;;
+    2)
+        subtraction $num1 $num2
+        ;;
+    3)
+        multiplication $num1 $num2
+        ;;
+    4)
+        division $num1 $num2
+        ;;
+    *)
+        echo "无效的计算方法"
+        ;;
+esac
+echo "计算器休息5秒，请快速记下答案！"
+sleep 5
+clear
+done
+            ;;
+        6)
+                  #加载程序
+echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "文件内容加载成功！"
+echo "请稍等"
+sleep 0.5
+        echo "你的ip是："
+curl http://myip4.ipip.net
+            ;;
+
+        7)
+                echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "文件内容加载成功！"
+echo "请稍等!"
+sleep 0.5
+            echo "退出成功"
+            break
+            ;;
+        *)
+            echo "无效的选项，请重新选择"
+            ;;
+    esac
+    
+            ;;
+        2)
+        #!/bin/bash
+
+#前言
+echo "Hello, this script is an open source script！"
+sleep 0.1
+echo "All content can be copied"
+sleep 0.1
+echo "Can't resell!"
+sleep 0.05
+echo "Can't resell!!"
+sleep 0.05
+echo "Can't resell!!!"
+sleep 0.1
+echo "Author Kuaishou：ba20111022"
+sleep 0.1
+echo "Next into the program"
+sleep 2
+clear
+#密码程序
+# 提示输入密码
+echo "Please enter your password："
+read -s password
+
+# 检查密码是否正确
+if [[ "$password" == "zhazhajun" ]]; then
+    # 提示登录成功
+    echo "Login Successfully！"
+else
+    # 如果密码错误，退出程序  
+    echo "Wrong password, program quit"
+    exit 1
+fi
+clear
+# 提示输入选项
+echo "Please select an option："
+    echo "1. time"
+    echo "2. calendar"
+    echo "3. 巴ba"
+    echo "4. Web Source Acquisition"
+    echo "5. calculator"
+    echo "6. IP acquisition"
+    echo "7. Sign out"
+
+    read choice
+clear
+    case $choice in
+        1)
+            #1
+    #加载程序
+echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File content loaded successfully！"
+echo "Just a moment, please!"
+sleep 0.5
+# 获取当前时间
+    current_time=$(date +%H:%M)
+#输出当前时间
+echo "The current time is：$(date +%H:%M)"
+echo "Loading successfully！"
+            ;;
+        2)
+            echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File content loaded successfully！"
+echo "Just a moment, please!"
+sleep 0.5
+cal
+echo "Loading successfully！"
+
+            ;;
+        3)
+        echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File content loaded successfully！"
+echo "Just a moment, please!"
+sleep 0.5
+             while true
+do
+echo "巴ba❤️nb666"#自己使用内容可更改
+set +x
+let "j=j+1"
+done
+            
+            ;;
+        4)
+            #加载程序
+echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File content loaded successfully！"
+echo "Just a moment, please!"
+sleep 0.5
+#!/bin/bash
+echo "Please enter the URL:"
+read url
+if [ $? -eq 0 ]; then
+curl -s "$url" > index.html
+echo "Source code is written index.html"
+else
+   echo "There was an error with the request!"
+fi
+            
+            ;;
+        5)
+        echo "Welcome to the calculator, the calculator loop execution!"
+        sleep 0.55
+        clear
+            echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File contentFile content loaded successfully!"
+echo "Just a moment, please!"
+sleep 0.5
+#!/system/bin/sh
+while true
+do
+# 定义函数实现加法
+addition() {
+    result=$(expr $1 + $2)
+    echo "Calculation results: $result"
+}
+
+# 定义函数实现减法
+subtraction() {
+    result=$(expr $1 - $2)
+    echo "Calculation results: $result"
+}
+
+# 定义函数实现乘法
+multiplication() {
+    result=$(expr $1 \* $2)
+    echo "Calculation results: $result"
+}
+
+# 定义函数实现除法
+division() {
+    result=$(echo "scale=2; $1 / $2" | bc)
+    echo "Calculation results: $result"
+}
+
+# 读取用户输入的两个数字
+echo "Please enter the first number.: "
+read num1
+echo "Please enter the second number.: "
+read num2
+
+# 显示操作选项给用户
+echo "Please select the calculation method："
+echo "1. addition"
+echo "2. subtraction"
+echo "3. multiplication"
+echo "4. division"
+
+# 读取用户选择的操作
+echo "Please enter the calculation method: "
+read operation
+
+# 根据用户选择的操作调用相应的函数进行计算并显示结果
+case $operation in
+    1)
+        addition $num1 $num2
+        ;;
+    2)
+        subtraction $num1 $num2
+        ;;
+    3)
+        multiplication $num1 $num2
+        ;;
+    4)
+        division $num1 $num2
+        ;;
+    *)
+        echo "Invalid calculation!"
+        ;;
+esac
+echo "Take 5 seconds off the calculator. Please jot down the answers quickly!"
+sleep 5
+clear
+done
+            ;;
+        6)
+                  #加载程序
+echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File content loaded successfully！"
+echo "Just a moment, please!"
+sleep 0.5
+        echo "Your IP is："
+curl http://myip4.ipip.net
+            ;;
+        7)
+            #加载程序
+echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File content loaded successfully！"
+echo "Just a moment, please!"
+sleep 0.5
+            echo "Sign out"
+            break
+            ;;
+        *)
+            echo "Invalid option, please re-select"
+            ;;
+    esac
+        
+            ;;
+        3)
+            #加载程序
+echo -ne '                 \033[1;95m  □□□□□□□□□□0% \r'
+sleep 0.10
+echo -ne '                 \033[1;95m  ■□□□□□□□□□10% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■□□□□□□□□20% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■□□□□□□□30% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■□□□□□□40% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■□□□□□50% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■□□□□60% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■□□□70% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■□□80% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■□90% \r'
+sleep 0.1
+echo -ne '                 \033[1;95m  ■■■■■■■■■■100% \r'
+sleep 0.1
+echo
+echo "File content loaded successfully！"
+echo "Just a moment, please!"
+sleep 0.5
+            echo "退出成功Exit successfully"
+            break
+            ;;
+        *)
+            echo "无效的选项，请重新选择(Invalid option, please re-select)"
+            ;;
+    esac
